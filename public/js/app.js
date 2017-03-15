03fbde99cb0d354bc732ee60cced38d047ff419a
Lauren Arfaras <larfaras96@gmail.com>
2017-03-15 19:41:58 -0400

    set up angular to work with the index.html page

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
-console.log('Working....');
+// immediately invoked functional expression
+(function(){
+  angular.module("MEANTodos", [])
+    .controller("TodoController", TodoController);
+
+  TodoController.$inject = ["$scope"];
+
+  function TodoController($scope){
+    $scope.message = "I WORK!";
+  }
+})()
